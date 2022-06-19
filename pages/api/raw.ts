8f585d6c09a8452e5b28449b6cb912f79fa34be3
Sparks <35142778+Montaro2017@users.noreply.github.com>
2022-06-19 13:44:46 +0800

    Update raw.ts

--- pages/api/raw.ts
+++ pages/api/raw.ts
@@ -82,7 +82,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       } else {
         let downloadUrl = data['@microsoft.graph.downloadUrl'];
         console.log("before", downloadUrl);
-        downloadUrl = downloadUrl.replace("montaro2017-my.sharepoint.com","od.montaro2017.workers.dev");
+        downloadUrl = downloadUrl.replace("montaro2017-my.sharepoint.com","od.cyck.tk");
         console.log("after", downloadUrl);
         res.redirect(downloadUrl)
       }
